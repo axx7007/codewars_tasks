f56b986c3c1696e0a6975acf950d2cdb671d785b
abduvaxobovxojiakbar7007 <xojiakbarabduvaxobov7007@gmail.com>
2022-03-07 16:43:06 +0500

    next task solved

--- js/index.js
+++ js/index.js
@@ -46,14 +46,28 @@
 // var arr=[1,2, 'a'];
 // console.log(arr.reduce((a,b)=>typeof a=='number'? a+b:a, 0).toString(2))
 
-var list1 = [
-    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
-  ];
+// var list1 = [
+//     { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+//     { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+//     { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+//   ];
 
-var newarr=list1.map((value)=>{
-    value.greeting=`Hi ${value.firstName}, what do you like the most about ${value.language}?`
+// var newarr=list1.map((value)=>{
+//     value.greeting=`Hi ${value.firstName}, what do you like the most about ${value.language}?`
+//     return value
+// })
+// console.log(newarr)
+var list = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+function isRubyComing(list) {
+var filtered=list.filter((value)=>{
+  if(value.language==='Ruby'){
     return value
+  }
 })
-console.log(newarr)
+ return filtered.length==1?true: false;
+}
+console.log(isRubyComing(list))
